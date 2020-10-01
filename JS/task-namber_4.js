a2663eeab4187e519a3b2a9542c662915ae71b74
Oleksandr Mykytiuk <66067417+AlexanderMykytiuk@users.noreply.github.com>
2020-10-01 16:39:59 +0300

    Add files via upload

---
+++ JS/task-namber_4.js
@@ -0,0 +1,28 @@
+const countTotalSalary = function(employees) {
+    'use strict';
+    // Write code under this line
+    let totalValue = 0;
+    
+    for (const value in employees) {
+      totalValue += employees[value];
+    };
+    return totalValue;
+  };
+  
+  // Объекты и ожидаемый результат
+  const developers = {
+      mango: 300,
+      poly: 250,
+      alfred: 450,
+  };
+  console.log(countTotalSalary(developers));
+  // 1000
+  
+  const supports = {
+    kiwi: 200,
+    lux: 150,
+    chelsy: 150,
+  }
+  console.log(countTotalSalary(supports));
+  // 500
+  
